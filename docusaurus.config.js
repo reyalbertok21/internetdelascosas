f7e08417ddddda899b358a999c76d58b6b181e7c
[Alberto <[rey.albertok@gmail.com]>
2023-05-05 11:15:03 +0200

    Cambios nuevos

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,17 +14,10 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://reyalbertok21.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/internetdelascosas',
-
-
-
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'reyalbertok21', // Usually your GitHub org/user name.
   projectName: 'internetdelascosas', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
